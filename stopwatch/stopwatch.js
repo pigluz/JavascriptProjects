8941b9006f39986382dedf1ef98cbf1b83003bbe
pigluz <franciszeklatawiec@gmail.com>
2023-04-09 12:30:45 +0200

    Add explanations

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -1,27 +1,35 @@
 window.onload = function () {
+
+  // Default values...
   var minutes = 00;
   var seconds = 00;
   var tens = 00;
 
+  // Gets values from spans
   var appendMinutes = document.getElementById("minutes");
   var appendTens = document.getElementById("tens");
   var appendSeconds = document.getElementById("seconds");
 
+  // Gets values from buttons
   var buttonStart = document.getElementById("button-start");
   var buttonStop = document.getElementById("button-stop");
   var buttonReset = document.getElementById("button-reset");
 
+  // Inicializes interval
   var Interval;
 
+  // If start button is clicked then stop time, and start the function called 'startTimer'
   buttonStart.onclick = function () {
     clearInterval(Interval);
     Interval = setInterval(startTimer, 10);
   };
 
+  // If stop button is clicked, stop time.
   buttonStop.onclick = function () {
     clearInterval(Interval);
   };
 
+  // If reset button is clicked, stop time, reset to the default values.
   buttonReset.onclick = function () {
     clearInterval(Interval);
     minutes = "00";
@@ -34,6 +42,7 @@ window.onload = function () {
   };
 
   function startTimer() {
+    // Everytime tens increases..
     tens++;
 
     if (tens <= 9) {
@@ -44,6 +53,7 @@ window.onload = function () {
       appendTens.innerHTML = tens;
     }
 
+    // If there is more than 99 tens, increase second by 1
     if (tens > 99) {
       console.log("seconds");
       seconds++;
@@ -56,6 +66,7 @@ window.onload = function () {
       appendSeconds.innerHTML = seconds;
     }
 
+    // If there is more than 60 seconds, increase minute by 1
     if (seconds >= 60) {
       minutes++;
       appendMinutes.innerHTML = "0" + minutes;
---
+++ timer/timer.js
@@ -0,0 +1,53 @@
+function setTimer() {
+
+    // Get time's values from the user
+  var yearU = document.getElementById("yearDate").value;
+  var monthU = document.getElementById("selectMonth").value;
+  var dayU = document.getElementById("dayDate").value;
+  var hourU = document.getElementById("hourDate").value;
+  var minuteU = document.getElementById("minuteDate").value;
+  var secondU = document.getElementById("secondDate").value;
+
+    // Convert those valeus into date
+  var countDownDate = new Date(
+    `${monthU} ${dayU}, ${yearU} ${hourU}:${minuteU}:${secondU}`
+  ).getTime();
+
+    // Start interval
+    var x = setInterval(function () {
+
+    // Gets date from now
+    var now = new Date().getTime();
+
+    // Calculates distance between user's date and now
+    var distance = countDownDate - now;
+
+    // Some crazy math floor..
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        
+    // Final result gets on the website
+    document.getElementById("timerText").innerHTML =
+    days + "<span id='timerTextDays'>d</span> " + 
+    hours + "<span id='timerTextHours'>h</span> " + 
+    minutes + "<span id='timerTextMinutes'>m</span> " + 
+    seconds + "<span id='timerTextSeconds'>s</span> ";
+  document.getElementById("timerTextDays").style.color = "grey";
+  document.getElementById("timerTextHours").style.color = "grey";
+  document.getElementById("timerTextMinutes").style.color = "grey";
+  document.getElementById("timerTextSeconds").style.color = "grey";
+  
+
+        
+    // If user enters a date that is already finished then..
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+}
